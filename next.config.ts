ed7d11c5be717975408b24f85a559bb2e08556fe
anthonyandres <anthonyandres1881@gmail.com>
2025-09-03 16:59:12 -0400

    add basepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  basePath: '/My-React-Website',
+  output: 'export',
 };
 
 export default nextConfig;
